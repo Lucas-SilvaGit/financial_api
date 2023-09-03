1d62affd39a94848c8b3d19baa973b38169855bd
Jose Lucas <lucasnba2324@gmail.com>
2023-09-03 09:23:16 -0300

    creating check account balance for not include  entries if value > balance

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,5 +2,26 @@ class Entry < ApplicationRecord
   belongs_to :account
   belongs_to :category
 
+  validate :check_account_balance, if: :despesa?
+
   enum entry_type: { receita: 'receita', despesa: 'despesa' }
+
+  # before_save :check_account_balance_if_billed_changed
+
+  private
+
+  def check_account_balance
+    return if account.balance > 0 && account.balance >= value
+
+    errors.add(:value, 'Expense amount exceeds account balance')
+  end
+
+  # def check_account_balance_if_billed_changed
+  #   if billed_changed? && billed
+  #     # return if account.balance >= 0 && account.balance >= value
+  #     return if account.balance >= value
+
+  #     errors.add(:value, 'Expense amount exceeds account balance')
+  #   end
+  # end
 end
