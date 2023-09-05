6f8ea8af4a20fd5d816e05fc0ea9b6f8e15e8773
Jose Lucas <lucasnba2324@gmail.com>
2023-09-05 20:32:56 -0300

    Adding validates to balance greater than or equal to 0

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,8 @@
     before_create :set_default_balance
     has_many :entries
 
+    validates :balance, numericality: { greater_than_or_equal_to: 0 }
+
     def calculate_balance
       total_expenses = entries.where(entry_type: 'expense', billed: true).sum(:value)
       total_revenue = entries.where(entry_type: 'revenue', billed: true).sum(:value)
