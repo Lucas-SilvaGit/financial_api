a75ff21eed2d5076d3e75e70d8a964f3ab7968bf
Jose Lucas <lucasnba2324@gmail.com>
2023-09-05 20:34:10 -0300

    creating tests for validates balance positive and negative

---
+++ spec/models/account_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Account, type: :model do
+  it "is valid with valid attributes" do
+    account = build(:account, balance: 1000)
+    expect(account).to be_valid
+  end
+
+  it "is invalid with a negative balance" do
+    account = build(:account, balance: -500)
+    expect(account).to_not be_valid
+  end
+end
