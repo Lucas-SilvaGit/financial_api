f6ee2a8c843bd51f3efff832c15b6dc049fb8f04
Jose Lucas <lucasnba2324@gmail.com>
2023-09-06 23:08:58 -0300

    Creating test for calculate balance correctly

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Account, type: :model do
     account = build(:account, balance: -500)
     expect(account).to_not be_valid
   end
+
+  it "calculates the balance correctly" do
+    account = create(:account)
+    create(:entry, account: account, entry_type: 'revenue', billed: true, value: 2000)
+    create(:entry, account: account, entry_type: 'expense', billed: true, value: 300)
+    
+    account.calculate_balance
+    expect(account.balance).to eq(1700)
+  end
 end
