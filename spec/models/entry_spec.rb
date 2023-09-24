176c15e6e450df95c9f4736b99dbce8550100075
Jose Lucas <lucasnba2324@gmail.com>
2023-09-24 19:07:46 -0300

    creating test for a raise an erro for entry negative value

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -23,5 +23,13 @@ RSpec.describe Entry, type: :model do
       
       expect { expense_entry.save! }.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Value Expense amount exceeds account balance')
     end
+
+    it 'when entry has negative value' do
+      account = create(:account, balance: 500)
+    
+      expense_entry = build(:entry, account: account, entry_type: 'expense', billed: false, value: -300)
+    
+      expect { expense_entry.save! }.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Value cannot be negative')
+    end
   end    
 end
