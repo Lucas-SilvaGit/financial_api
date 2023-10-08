fc7eb2d3c766892883a463a30131fd921fa2d758
Jose Lucas <lucasnba2324@gmail.com>
2023-10-08 20:33:11 -0300

    fixing comments

--- app/controllers/v1/dashboard_controller.rb
+++ app/controllers/v1/dashboard_controller.rb
@@ -7,30 +7,30 @@ module V1
       # Formata o mês com dois dígitos
       formatted_month = format('%02d', month)
 
-      # Consulta para calcular o total de receitas no mês e ano especificados
+      # Consulta para calcular o total de RECEITAS no mês e ano especificados
       total_revenues = Entry.where(entry_type: 'revenue', billed: true)
-                           .where("strftime('%Y', date) = ? AND strftime('%m', date) = ?", year.to_s, formatted_month)
-                           .sum(:value)
+                            .where("strftime('%Y', date) = ? AND strftime('%m', date) = ?", year.to_s, formatted_month)
+                            .sum(:value)
 
-      # Consulta para calcular o total de receitas previsto no mês e ano especificados
+      # Consulta para calcular o total de RECEITAS PREVISTO no mês e ano especificados
       total_revenues_expected = Entry.where(entry_type: 'revenue')
                                     .where("strftime('%Y', date) = ? AND strftime('%m', date) = ?", year.to_s, formatted_month)
                                     .sum(:value)
 
-      # Consulta para calcular o total de despesas no mês e ano especificados
+      # Consulta para calcular o total de DESPESAS no mês e ano especificados
       total_expenses = Entry.where(entry_type: 'expense', billed: true)
                             .where("strftime('%Y', date) = ? AND strftime('%m', date) = ?", year.to_s, formatted_month)
                             .sum(:value)
 
-      # Consulta para calcular o total de despesas previsto no mês e ano especificados
+      # Consulta para calcular o total de DESPESAS PREVISTO no mês e ano especificados
       total_expenses_expected = Entry.where(entry_type: 'expense')
                                     .where("strftime('%Y', date) = ? AND strftime('%m', date) = ?", year.to_s, formatted_month)
                                     .sum(:value)
 
-      # Calculo do saldo total das contas no mês e ano especificados
+      # Calculo do SALDO TOTAL das contas no mês e ano especificados
       balance_total = total_revenues - total_expenses
 
-      # Calculo do saldo total das contas PREVISTO no mês e ano especificados
+      # Calculo do SALDO TOTAL das contas PREVISTO no mês e ano especificados
       balance_total_expected = total_revenues_expected - total_expenses_expected
 
       render json: {
