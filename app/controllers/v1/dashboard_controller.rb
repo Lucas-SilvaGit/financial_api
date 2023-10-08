bc693736c2ddfe5fc6eb54eafc1cc8e5702c9f77
Jose Lucas <lucasnba2324@gmail.com>
2023-10-08 20:23:30 -0300

    creating query to search for total expected revenue

--- app/controllers/v1/dashboard_controller.rb
+++ app/controllers/v1/dashboard_controller.rb
@@ -12,18 +12,24 @@ module V1
                            .where("strftime('%Y', date) = ? AND strftime('%m', date) = ?", year.to_s, formatted_month)
                            .sum(:value)
 
+      # Consulta para calcular o total de receitas previsto no mês e ano especificados
+      total_revenue_expected = Entry.where(entry_type: 'revenue')
+                                    .where("strftime('%Y', date) = ? AND strftime('%m', date) = ?", year.to_s, formatted_month)
+                                    .sum(:value)
+
       # Consulta para calcular o total de despesas no mês e ano especificados
       total_expenses = Entry.where(entry_type: 'expense', billed: true)
                             .where("strftime('%Y', date) = ? AND strftime('%m', date) = ?", year.to_s, formatted_month)
                             .sum(:value)
 
-      # Calculo do saldo total da conta no mês e ano especificados
-      saldo_total = total_revenue - total_expenses
+      # Calculo do saldo total das contas no mês e ano especificados
+      balance_total = total_revenue - total_expenses
 
       render json: {
-        totalReceitas: total_revenue,
-        totalDespesas: total_expenses,
-        saldoTotal: saldo_total
+        totalRevenues: total_revenue,
+        totalRevenuesExpected: total_revenue_expected,
+        totalExpenses: total_expenses,
+        balanceTotal: balance_total
       }
     end
   end
