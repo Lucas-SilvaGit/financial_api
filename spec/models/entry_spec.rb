edb59bd0043243c640d5f87c2bdffa051865d6ed
Jose Lucas <lucasnba2324@gmail.com>
2023-11-16 22:14:50 -0300

    creating test to calculate balance correctly for revenue

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  let(:account) { create(:account, balance: 1000) }
+  let(:account) { create(:account, balance: 0) }
+
+  it 'calculates the account balance correctly for expense entries' do
+    entry = create(:entry, account: account, entry_type: 'revenue', billed: true, value: 300)
+    account.calculate_balance
+
+    expect(account.reload.balance).to eq(300)
+  end
 
   it "displays an error message if the expense exceeds the account balance" do
     entry = build(:entry, account: account, entry_type: 'expense', billed: true, value: 1500)
@@ -11,6 +18,7 @@ RSpec.describe Entry, type: :model do
   end
 
   it "does not display an error message if the expense does not exceed the account balance" do
+    account = create(:account, balance: 700)
     entry = build(:entry, account: account, entry_type: 'expense', billed: true, value: 500)
 
     expect(entry).to be_valid
